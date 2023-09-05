cc52838925c4a2ef3efa4f28c9085668b1ec71f7
Ludvig Gunne Lindström <ludvig.gunne.lindstrom.dev@proton.me>
2023-09-05 16:51:38 +0200

    Testing exception handling

--- src/main.zig
+++ src/main.zig
@@ -14,6 +14,9 @@ comptime { asm ( ".section .text" ); }
 // Kernel
 export fn kmain() noreturn {
     uart.init();
-    asm volatile ( "svc 0x0" );
+    uart.writer.print("UART initialized\n", .{}) catch unreachable;
+    asm volatile ( "svc 0x69" );
+    uart.writer.print("Exception should have been handled by now..\n", .{})
+        catch unreachable;
     while (true) {}
 }
